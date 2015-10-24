8a11644dcdea8ee3215ba7ed9f3f827147dd91c3
Andrew Chester <thechest4@gmail.com>
2015-10-24 18:52:53 -0400

    Death UI

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -3,12 +3,26 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour
 {
+	[SerializeField]
+	private GameObject deathUIContent;
+
 	void Awake ()
 	{
 		PlayerMove.PlayerDeath += HandlePlayerDeath;
 	}
 
 	void HandlePlayerDeath ()
+	{
+		PlayerMove.PlayerDeath -= HandlePlayerDeath;
+		if (deathUIContent != null) 
+		{
+			deathUIContent.SetActive (true);
+		}
+
+		Invoke ("LoadNewLevel", 4.0f);
+	}
+
+	private void LoadNewLevel ()
 	{
 		Application.LoadLevel ("this");
 	}
